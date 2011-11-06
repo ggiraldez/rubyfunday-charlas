3d7980eda45c0971175f502f95245d92c4161dae
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2011-11-06 14:25:38 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charlas::Application.config.secret_token = '9a51b3ceebb6e926100f0002e3fe045a7c72d1ab393ed2f8e079f9626c333be009cdda8b2c5200a3d1a17dc8ad4cb8e98267cff4a410a879b215cebc8276b016'
